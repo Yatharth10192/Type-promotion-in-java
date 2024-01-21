0bcb862284cc3c3cfc26f06e13c2e123f883abfd
Yatharth <Yatharth@DESKTOP-JKKRKNV>
2024-01-21 14:17:25 +0530

    Type promotion in java

---
+++ TypePromotion/src/demo.java
@@ -0,0 +1,10 @@
+public class demo {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 12.25f;
+        long c = 25;
+        double d = 30;
+        double ans = a + b + c + d;
+        System.out.println(ans);
+    }
+}
